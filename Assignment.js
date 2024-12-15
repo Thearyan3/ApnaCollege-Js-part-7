bf5393e9160aad310c386ccf4b125a556ad5c7b5
Aryan <chaudharyaryan3456@gmail.com>
2024-12-15 19:50:01 +0530

    updated

--- Assignment.js
+++ Assignment.js
@@ -24,10 +24,26 @@
 
 
 // Qs3 . What is the output of the following code :
+// const object = {
+//     message: 'Hello,World!',
+//     logMessage() { 
+//         console.log(this.message);
+//     }
+//  };
+// setTimeout(object.logMessage,1000);
+
+
+
+
+// Qs4 . What is the output of the following code:
+let length = 4;
+function callback() {
+    console.log(this.length);
+}
 const object = {
-    message: 'Hello,World!',
-    logMessage() { 
-        console.log(this.message);
-    }
- };
-setTimeout(object.logMessage,1000);
+    length: 5,
+    method(callback){
+        callback();
+    },
+};
+object.method(callback,1,2);
