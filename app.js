7458b7a473d01f650bb52b8506dd4ef497ed157d
Aryan <chaudharyaryan3456@gmail.com>
2024-12-15 18:18:27 +0530

    updated

--- app.js
+++ app.js
@@ -16,3 +16,9 @@
 
 // try&catch - The try statement allows you to define a block of code to be tested for errors while it is being executed.
 // The catch statement allows you to define a block of code to be executed, if an error occurs in try block. 
+try {
+    console.log(a);
+} catch(err) {
+    console.log("caught error .. a is not defined");
+    console.log(err);
+}
