cb627bc55fa3816d69b0b579871a27c947df3345
Aryan <chaudharyaryan3456@gmail.com>
2024-12-15 19:42:54 +0530

    updated

--- Assignment.js
+++ Assignment.js
@@ -8,3 +8,26 @@
 // console.log(sum/arr.length);
 // }
 // console.log(arrayAverage(arr));
+
+
+
+// Qs 2. Write an arrow function named isEven() that takes a single number as an argument and returns if it is even or not.
+// let isEven = (n) => {
+//     if(n%2 == 0){
+//         console.log(`${n} is even.`);
+//     } else {
+//         console.log(`${n} is odd.`);
+//     }
+// }
+// console.log(isEven(4));
+
+
+
+// Qs3 . What is the output of the following code :
+const object = {
+     message: 'Hello,World!',
+      logMessage() { 
+        console.log(this.message);
+    }
+ };
+setTimeout(object.logMessage,1000);
