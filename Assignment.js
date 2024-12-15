0139059ee7e84efbd48e697016272f09a6bd83ce
Aryan <chaudharyaryan3456@gmail.com>
2024-12-15 19:37:58 +0530

    updated

---
+++ Assignment.js
@@ -0,0 +1,10 @@
+// Qs 1. Write an arrow function named arrayAverage that accepts array of numbers and returns the average of those numbers.
+// let arr = [1, 2, 3, 5, 4];
+// let arrayAverage = (arr) => {
+// let sum = 0;
+// for(n of arr){
+//     sum += n;
+// }
+// console.log(sum/arr.length);
+// }
+// console.log(arrayAverage(arr));
